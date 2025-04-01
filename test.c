9bb62f27a29c7c99801c9582071204d566f5fd7c
sherbs <59579863+sheebos@users.noreply.github.com>
2025-04-01 09:54:21 -0400

    updated outline

---
+++ test.c
@@ -0,0 +1,13 @@
+//this file is going to be converted to web assembly, since c is an easier syntax to handle
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(int arg1, int arg2){
+    //printf("Hello World\n");
+    //printf("%d\n", arg1);
+    //printf("%d\n", arg2);
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int l = 0;
+}
